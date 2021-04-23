2a3e88cdf11b919d064a9dba8ba6cc3e49e612ff
Kevin Conner <connerk@gmail.com>
2021-04-23 18:09:43 -0400

    Initial Commit

---
+++ CarPlayExample/CarPlayExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarPlayExampleApp.swift
+//  CarPlayExample
+//
+//  Created by Kevin Conner on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct CarPlayExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
